3972fde02998e9b575c3266cde971c0d5b58c2fe
Your Name <youremail@example.com>
2025-04-19 18:13:12 +0100

    changed ami id in ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
+    ami           = "ami-08f78cb3cc8a4578e" # Amazon Linux 2 AMI
     instance_type = "t2.micro"
 
     user_data = <<-EOF
