ede16c0cd6173e069e6b116621bb3f39e73e85f1
Your Name <youremail@example.com>
2025-04-20 17:03:40 +0100

    removed the resource code in security.tf  as the security group has already been created

--- security_group.tf
+++ security_group.tf
@@ -6,41 +6,12 @@ data "aws_security_group" "existing_web_sg" {
   }
 }
 
-
-resource "aws_security_group" "web_sg" {
-    name        = "web-sg"
-    description = "Allow inbound and outbound traffic on port 80"
-    vpc_id      = "vpc-08b58ef13cb1bb33d" # Replace with your VPC ID
-  
-    ingress {
-        description = "Allow SSH"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        description = "Allow inbound HTTP traffic"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        description = "Allow outbound HTTP traffic"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
 resource "aws_instance" "web_instance" {
     ami = "ami-08f78cb3cc8a4578e" # Amazon Linux 2 AMI
     instance_type = "t3.micro"
-    security_groups = [aws_security_group.web_sg.name]
+    
+     # Reference the existing security group from the data block
+    security_groups = [data.aws_security_group.existing_web_sg.name]
 
     tags = {
         Name = "WebServer"
